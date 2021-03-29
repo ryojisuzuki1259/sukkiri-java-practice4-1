6991be27a9b03149ab618dd87de8f252b863a233
suzukiryoji <suzukiryoji@localhost>
2021-03-29 18:32:50 +0900

    Write answers

--- src/main.java
+++ src/main.java
@@ -2,8 +2,10 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
 	}
 
 }
